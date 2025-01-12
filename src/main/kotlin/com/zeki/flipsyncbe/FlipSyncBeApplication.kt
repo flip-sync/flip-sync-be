9b8b0b6b2bc0c17699d93ae800206f18b47a7986
ojy9612 <ojy9612@gmail.com>
2025-01-12 20:37:15 +0900

    init

---
+++ src/main/kotlin/com/zeki/flipsyncbe/FlipSyncBeApplication.kt
@@ -0,0 +1,11 @@
+package com.zeki.flipsyncbe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FlipSyncBeApplication
+
+fun main(args: Array<String>) {
+    runApplication<FlipSyncBeApplication>(*args)
+}
