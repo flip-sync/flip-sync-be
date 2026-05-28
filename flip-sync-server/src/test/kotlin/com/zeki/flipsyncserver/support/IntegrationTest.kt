e25cfa3adb9152dc138084c3a76ab8d4497db9d3
jangkyoungtae <49186181+jangkyoungtae@users.noreply.github.com>
2026-05-28 11:00:01 +0900

    Force H2 for integration tests

--- flip-sync-server/src/test/kotlin/com/zeki/flipsyncserver/support/IntegrationTest.kt
+++ flip-sync-server/src/test/kotlin/com/zeki/flipsyncserver/support/IntegrationTest.kt
@@ -6,6 +6,11 @@ import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest(
     properties = [
+        "spring.custom.datasource.driver-class-name=org.h2.Driver",
+        "spring.custom.datasource.url=jdbc:h2:mem:testdb;MODE=MySQL;DB_CLOSE_DELAY=-1;DATABASE_TO_LOWER=TRUE;INIT=CREATE SCHEMA IF NOT EXISTS flip_sync",
+        "spring.custom.datasource.username=sa",
+        "spring.custom.datasource.password=",
+        "spring.jpa.hibernate.ddl-auto=create-drop",
         "flipsync.local-upload.enabled=true",
         "flipsync.local-upload.base-url=http://localhost:8080"
     ]
